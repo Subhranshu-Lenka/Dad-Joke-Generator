968106cc1d16e66f035e5dd52c1d953a18bbf68c
Subhranshu-Lenka <128354031+Subhranshu-Lenka@users.noreply.github.com>
2023-03-20 12:31:31 +0530

    Add files via upload

---
+++ joke generated/joke.js
@@ -0,0 +1,20 @@
+const button = document.querySelector(".container button");
+const jokeText = document.querySelector(".container p");
+
+document.addEventListener('DOMContentLoaded', getJoke);
+
+button.addEventListener('click', getJoke);
+
+async function getJoke() {
+    const jokeData = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+    });
+
+    const jokeObj = await jokeData.json();
+
+    // console.log(jokeObj);
+    jokeText.innerHTML = jokeObj.joke;
+
+}
